2931098da49f785345080df9dd775cea6ccf64f0
John Sutherland <john@orion.lan>
2009-10-04 11:23:39 +0100

    A bunch of working stuff.

--- mnmlnws.rb
+++ mnmlnws.rb
@@ -1,5 +1,27 @@
+require 'rubygems'
 require 'sinatra'
- 
+require 'haml'
+require 'rss'
+
+
+set :haml, {:format => :html5 }
+
+
+FEEDS = {
+  'front' => 'http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/front_page/rss.xml',
+  'scotland' => 'http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/scotland/rss.xml',
+  'technology' => 'http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/technology/rss.xml',
+}
+
+
 get '/' do
-  "Hello World!"
+  redirect '/front' # Lazy
+end
+
+
+get '/:feed' do
+  feed = FEEDS[params[:feed]]
+  rss = RSS::Parser.parse(feed)
+  @items = rss.items[0, 5]
+  haml :index
 end
